ab801d28d1666adefe88cc22665ee42fc79e021b
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-05-23 15:59:34 -0400

    Finished.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double ccBalance = 5000.00;
+        final double INTEREST_RATE = 0.17;
+        double oneMonthInterest = ccBalance * INTEREST_RATE;
+        double twoMonthInterest = (ccBalance + oneMonthInterest) * INTEREST_RATE;
+
+        System.out.println("The interest due after one month is $" + oneMonthInterest + " and the interest due after two months is $" + twoMonthInterest);
     }
 }
